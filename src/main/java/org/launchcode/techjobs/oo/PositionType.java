208c3bf993a7ca9055ffc3dfaf5098843c7c93e1
Ingrid Barrett <barrett92@yahoo.com>
2022-02-02 18:44:48 -0500

    First Commit - sections 1-3

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -17,9 +17,26 @@ public class PositionType {
     }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
+    @Override
+    public String toString() { return value;}
+
 
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+
+    @Overide
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof PositionType)) return false;
+        if (!super.equals(object)) return false;
+        PositionType that = (PositionType) object;
+        return getId() == that.getId();
+    }
+
+    public int hashCode() {
+        return java.util.Objects.hash(super.hashCode(), getId());
+    }
+
 
     // Getters and Setters:
 
